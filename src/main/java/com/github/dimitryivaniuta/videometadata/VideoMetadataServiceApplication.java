83096aa09b82abfeaeabf0ae8b396e704ccc9fb6
Dimitry Ivaniuta <dzmitry.ivaniuta.services@gmail.com>
2025-09-05 14:01:38 +0200

    Video Metadata service application update

--- src/main/java/com/github/dimitryivaniuta/videometadata/VideoMetadataServiceApplication.java
+++ src/main/java/com/github/dimitryivaniuta/videometadata/VideoMetadataServiceApplication.java
@@ -3,7 +3,6 @@ package com.github.dimitryivaniuta.videometadata;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 
 /**
